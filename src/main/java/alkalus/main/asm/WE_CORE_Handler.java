a31aab0b40e1ad88c82b4da3a73f2d5288010dae
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-08-06 18:54:26 +0200

    move ASMConfig init to a static block

--- src/main/java/alkalus/main/asm/WE_CORE_Handler.java
+++ src/main/java/alkalus/main/asm/WE_CORE_Handler.java
@@ -1,26 +1,15 @@
 package alkalus.main.asm;
 
-import java.io.File;
-
 import net.minecraft.launchwrapper.IClassTransformer;
 
 import alkalus.main.asm.transformer.ClassTransformer_Witchery_NEIWitcheryConfig;
 
-@SuppressWarnings("static-access")
 public class WE_CORE_Handler implements IClassTransformer {
-
-    public static final AsmConfig mConfig;
-
-    static {
-        mConfig = new AsmConfig(new File("config/WitcheryExtras/asm.cfg"));
-        System.out
-                .println("[Witchery++][ASM] Asm Config Location: " + mConfig.config.getConfigFile().getAbsolutePath());
-    }
 
     @Override
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
         // Fix Bad NEI Handling, by patching it out entirely.
-        if (transformedName.equals("com.emoniph.witchery.integration.NEIWitcheryConfig") && mConfig.enablePatchNEI) {
+        if (transformedName.equals("com.emoniph.witchery.integration.NEIWitcheryConfig") && AsmConfig.enablePatchNEI) {
             return new ClassTransformer_Witchery_NEIWitcheryConfig(
                     transformedName,
                     basicClass,

1da171593f8ddc0ac37ec8763f4b281580b0b23f
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-08-07 00:34:19 +0200

    delete useless proxies

--- src/main/java/alkalus/main/core/WitcheryExtras.java
+++ src/main/java/alkalus/main/core/WitcheryExtras.java
@@ -4,12 +4,15 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.minecraftforge.common.MinecraftForge;
+
 import alkalus.main.api.plugin.base.BasePluginWitchery;
+import alkalus.main.core.crafting.OvenRecipes;
 import alkalus.main.core.entities.PredictionHandler;
-import alkalus.main.core.proxy.Proxy_Common;
+import alkalus.main.core.recipe.fixes.GarlicRecipes;
 import alkalus.main.core.util.Logger;
+import alkalus.main.core.util.TooltipHandler;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -26,29 +29,16 @@ public class WitcheryExtras {
     public static final String VERSION = "GRADLETOKEN_VERSION";
     private static final Logger log4j = new Logger();
 
-    private static final Map<Integer, BasePluginWitchery> mPreInitEvents;
-    private static final Map<Integer, BasePluginWitchery> mInitEvents;
-    private static final Map<Integer, BasePluginWitchery> mPostInitEvents;
-
-    // Static Initialization block
-    static {
-        mPreInitEvents = new HashMap<Integer, BasePluginWitchery>();
-        mInitEvents = new HashMap<Integer, BasePluginWitchery>();
-        mPostInitEvents = new HashMap<Integer, BasePluginWitchery>();
-    }
+    private static final Map<Integer, BasePluginWitchery> mPreInitEvents = new HashMap<>();
+    private static final Map<Integer, BasePluginWitchery> mInitEvents = new HashMap<>();
+    private static final Map<Integer, BasePluginWitchery> mPostInitEvents = new HashMap<>();
 
     @Mod.Instance(MODID)
     public static WitcheryExtras instance;
 
-    @SidedProxy(
-            clientSide = "alkalus.main.core.proxy.Proxy_Client",
-            serverSide = "alkalus.main.core.proxy.Proxy_Server")
-    public static Proxy_Common proxy;
-
     @Mod.EventHandler
     public synchronized void preInit(final FMLPreInitializationEvent e) {
         log(0, "Loading " + NAME + " - v" + VERSION);
-        proxy.preInit(e);
         for (BasePluginWitchery bwp : getMpreinitevents()) {
             log(0, "Loading Plugin: " + bwp.getPluginName() + " | Phase: Pre-Init");
             bwp.preInit();
@@ -57,17 +47,19 @@ public class WitcheryExtras {
 
     @Mod.EventHandler
     public synchronized void init(final FMLInitializationEvent e) {
-        proxy.init(e);
+        new GarlicRecipes();
+        OvenRecipes.generateDefaultOvenRecipes();
         for (BasePluginWitchery bwp : getMinitevents()) {
             log(0, "Loading Plugin: " + bwp.getPluginName() + " | Phase: Init");
             bwp.init();
         }
-        proxy.registerRenderers();
     }
 
     @Mod.EventHandler
-    public synchronized void postInit(final FMLPostInitializationEvent e) {
-        proxy.postInit(e);
+    public synchronized void postInit(final FMLPostInitializationEvent event) {
+        if (event.getSide().isClient()) {
+            MinecraftForge.EVENT_BUS.register(new TooltipHandler());
+        }
         for (BasePluginWitchery bwp : getMpostinitevents()) {
             log(0, "Loading Plugin: " + bwp.getPluginName() + " | Phase: Post-Init");
             bwp.postInit();

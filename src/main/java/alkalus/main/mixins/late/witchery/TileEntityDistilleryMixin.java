ad766d7ef41e7204a7a1cb27f5781dce61b226d2
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-11-30 12:47:55 -0500

    Fix remapping in dev *and* prod

--- src/main/java/alkalus/main/mixins/late/witchery/TileEntityDistilleryMixin.java
+++ src/main/java/alkalus/main/mixins/late/witchery/TileEntityDistilleryMixin.java
@@ -1,16 +1,24 @@
 package alkalus.main.mixins.late.witchery;
 
+import net.minecraft.inventory.IInventory;
+
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
 
 import com.emoniph.witchery.blocks.BlockDistillery;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 @SuppressWarnings("UnusedMixin")
-@Mixin(BlockDistillery.TileEntityDistillery.class)
-public class TileEntityDistilleryMixin {
+@Mixin(value = BlockDistillery.TileEntityDistillery.class, remap = false)
+public abstract class TileEntityDistilleryMixin implements IInventory {
 
-    @ModifyConstant(method = "isItemValidForSlot", constant = @Constant(intValue = 3), remap = false)
+    // Manually specify obfuscation mapping, and require that exactly one hits.
+    // The mixin AP can't find it automatically for some reason
+    @ModifyExpressionValue(
+            method = { "isItemValidForSlot", "func_94041_b" },
+            at = @At(value = "CONSTANT", args = "intValue=3"),
+            require = 1,
+            allow = 1)
     private int witcheryextras$fixSlotInsertion(int original) {
         return 2;
     }

ed3ec337de3e578eaf267f0415f3cfa5987bc559
tiffit <10173431+tiffit@users.noreply.github.com>
2026-05-05 09:44:12 -0700

    Fix witchery trying to be loaded in early stage (#40)

--- src/main/java/alkalus/main/mixins/Mixins.java
+++ src/main/java/alkalus/main/mixins/Mixins.java
@@ -12,27 +12,21 @@ public enum Mixins implements IMixins {
     // spotless:off
     FIX_SHAPESHIFT_SYNC(new MixinBuilder("Correct sync offset when shapeshift")
             .addClientMixins("minecraft.NetHandlerPlayClientMixin")
-            .addRequiredMod(TargetedMod.WITCHERY)
             .setPhase(Phase.EARLY)),
     FIX_SHAPESHIFT_STANCE_VALIDATION(new MixinBuilder("Correct stance limit when shapeshift")
             .addCommonMixins("minecraft.NetHandlerPlayServerMixin")
-            .addRequiredMod(TargetedMod.WITCHERY)
             .setPhase(Phase.EARLY)),
     FIX_IS_ENTITY_INSIDE_OPAQUE_BLOCK(new MixinBuilder("Fix suffocation check for resized entities")
             .addCommonMixins("minecraft.EntityMixin")
-            .addRequiredMod(TargetedMod.WITCHERY)
             .setPhase(Phase.EARLY)),
     FIX_POSITON_RESIZING_EYE_HEIGHT(new MixinBuilder("Fix player eye height positioning during resizing")
             .addCommonMixins("witchery.PotionResizingMixin")
-            .addRequiredMod(TargetedMod.WITCHERY)
             .setPhase(Phase.LATE)),
     FIX_SHAPESHIFT_CAMERA(new MixinBuilder("Correct camera offset when shapeshift")
             .addClientMixins("minecraft.EntityRendererMixin")
-            .addRequiredMod(TargetedMod.WITCHERY)
             .setPhase(Phase.EARLY)),
     WITCHERY_OFFSET_CAPTURE(new MixinBuilder("Skips the original view shifting logic")
             .addClientMixins("witchery.PlayerRenderMixin")
-            .addRequiredMod(TargetedMod.WITCHERY)
             .setPhase(Phase.LATE)),
     WITCHERY(new MixinBuilder()
             .addCommonMixins(
@@ -45,24 +39,20 @@ public enum Mixins implements IMixins {
                     "witchery.ItemGeneral$3$1Mixin",
                     "witchery.BlockAltarMixin")
             .addClientMixins("witchery.ItemBrewMixin")
-            .addRequiredMod(TargetedMod.WITCHERY)
             .setPhase(Phase.LATE)),
     WITCHERY_NEI_CONFIG(new MixinBuilder()
             .addCommonMixins("witchery.NEIWitcheryConfigMixin")
             .setApplyIf(()-> AsmConfig.enablePatchNEI)
-            .addRequiredMod(TargetedMod.WITCHERY)
             .setPhase(Phase.LATE)),
     WITCHERY_BAUBLES_COMPAT(new MixinBuilder()
             .addCommonMixins(
                     "witchery.BlockWitchDoorMixin_Bauble",
                     "witchery.ItemGeneralMixin_Bauble",
                     "witchery.ItemPoppetMixin_Bauble")
-            .addRequiredMod(TargetedMod.WITCHERY)
             .addRequiredMod(TargetedMod.BAUBLES_EXPANDED)
             .setPhase(Phase.LATE)),
     WITCHERY_DISABLE_CONTAINER_BLOCKS(new MixinBuilder()
             .addCommonMixins("witchery.BlockBaseContainerMixin")
-            .addRequiredMod(TargetedMod.WITCHERY)
             .setPhase(Phase.LATE)
             // configs handle individual blocks to disable
             ),
--- src/main/java/alkalus/main/mixins/TargetedMod.java
+++ src/main/java/alkalus/main/mixins/TargetedMod.java
@@ -7,17 +7,12 @@ import com.gtnewhorizon.gtnhmixins.builders.TargetModBuilder;
 
 public enum TargetedMod implements ITargetMod {
 
-    WITCHERY(null, "witchery", "com.emoniph.witchery.Witchery"),
     BAUBLES_EXPANDED("Baubles|Expanded");
 
     private final TargetModBuilder builder;
 
     TargetedMod(String modId) {
         this.builder = new TargetModBuilder().setModId(modId);
-    }
-
-    TargetedMod(String coreModClass, String modId, String targetClass) {
-        this.builder = new TargetModBuilder().setCoreModClass(coreModClass).setModId(modId).setTargetClass(targetClass);
     }
 
     @Nonnull

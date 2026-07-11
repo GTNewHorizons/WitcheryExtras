6de9617c8dcaae93703a821f434c558d62077131
hinyb <40139991+hinyb@users.noreply.github.com>
2026-07-11 23:14:22 +0800

    fix(shapeshift): fix swimming detection when shapeshift

--- src/main/java/alkalus/main/mixins/early/minecraft/EntityMixin.java
+++ src/main/java/alkalus/main/mixins/early/minecraft/EntityMixin.java
@@ -5,6 +5,8 @@ import net.minecraft.entity.player.EntityPlayer;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
@@ -13,6 +15,14 @@ public abstract class EntityMixin {
 
     @ModifyExpressionValue(method = "isEntityInsideOpaqueBlock", at = @At(value = "CONSTANT", args = "floatValue=0.1F"))
     private float applyOffset(float origin) {
+        if ((Object) this instanceof EntityPlayer player) {
+            return origin * player.height / 1.8F;
+        }
+        return origin;
+    }
+
+    @ModifyConstant(method = "handleWaterMovement", constant = @Constant(doubleValue = -0.4000000059604645D))
+    private double etfu$keepSwimmingPlayersInWater(double origin) {
         if ((Object) this instanceof EntityPlayer player) {
             return origin * player.height / 1.8F;
         }
